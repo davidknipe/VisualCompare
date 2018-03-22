cb3c04e316d4d5d8ec008f3341c9b83087910ac0
David Knipe <david.knipe@episerver.com>
2018-03-22 10:36:52 +0000

    Update to ensure module is discovered on initialisation

--- Nuget/pack.bat
+++ Nuget/pack.bat
@@ -6,6 +6,7 @@ del /Q Package\content\*.*
 @Echo Copying new files
 xcopy ..\VisualCompareMode\bin\Release\VisualCompareMode.dll Package\lib\net45
 xcopy ..\VisualCompareMode\modules\_protected\VisualCompareMode\*.*  Package\content\modules\_protected\VisualCompareMode\ /S /Y
+xcopy ..\VisualCompareMode\web.config.transform Package\content
 
 @Echo Packing files
 "..\.nuget\nuget.exe" pack package\VisualCompareMode.nuspec
