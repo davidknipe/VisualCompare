8a33b07d94610da746fd01682f762d3fa3a31b2d
Matthew Jimenez <mjimenez@cmh.edu>
2022-05-10 12:53:43 -0500

    First attempt to support CMS 12 - comparing properties appears to work. On page compare may need adjustment

---
+++ VisualCompareMode/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+#if NET
+using EPiServer.Shell.Modules;
+using Microsoft.Extensions.DependencyInjection;
+#else
+using EPiServer.Shell.Modules.Internal;
+using EPiServer.ServiceLocation;
+using IServiceCollection = EPiServer.ServiceLocation.IServiceConfigurationProvider;
+#endif
+
+namespace VisualCompareMode
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddVisualCompareMode(this IServiceCollection services)
+        {
+            return services.Configure<ProtectedModuleOptions>(pmo =>
+            {
+                if (!pmo.Items.Any(x => x.Name.Equals("VisualCompareMode")))
+                {
+                    pmo.Items.Add(new ModuleDetails { Name = "VisualCompareMode" });
+                }
+            });
+        }
+    }
+}
